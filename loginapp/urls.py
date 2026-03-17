2c5a5d49fb861eb7552f081ff2bb181ad7f05a10
Winar Owen <owensumargo66@gmail.com>
2026-03-18 00:23:38 +0700

    Added create blog and view blog feature

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -4,5 +4,7 @@ urlpatterns = [
     path('register/',views.register,name='register'),
     path('login/',views.user_login,name='login'),
     path('',views.home,name='home'),
+    path('create_post/',views.create_blog,name='create_blog'),
+    path('view_post',views.view_blog,name='view_blog'),
     
 ]
--- loginapp/views.py
+++ loginapp/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render,redirect
 from .forms import Registration,BlogForm
 from django.contrib.auth import login
 from django.contrib.auth.forms import AuthenticationForm
-
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from .models import Post
 # Create your views here.
 def register(request):
     if request.method == 'POST':
@@ -28,3 +30,25 @@ def user_login(request):
 
 def home(request):
     return render(request,'home/home.html')
+
+@login_required
+def create_blog(request):
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            blog = form.save(commit=False)
+            blog.author = request.user
+            blog.save()
+            return redirect('home')
+    else:
+        form = BlogForm()
+    return render(request,'create_blog/create_blog.html',{'form':form})
+
+def view_blog(request):
+    blog_list = Post.objects.all().order_by('-created_at')
+    paginator = Paginator(blog_list,5)
+    page_number = request.GET.get('page')
+    blogs = paginator.get_page(page_number)
+
+    return render(request,'view_blog/view_blog.html',{'blogs':blogs})
+
