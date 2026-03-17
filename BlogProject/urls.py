8312b0a1a8165a46b7bf2d1f2b3a98c15a9c00ef
Winar Owen <owensumargo66@gmail.com>
2026-03-17 23:06:37 +0700

    Remove unnecessary URL in project urls.py

--- BlogProject/urls.py
+++ BlogProject/urls.py
@@ -19,7 +19,6 @@ from django.urls import path,include
 from loginapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home,name='home'),
     path('',include('loginapp.urls')),
     
 ]
