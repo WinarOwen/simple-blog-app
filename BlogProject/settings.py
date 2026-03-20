f3c58cd2e561df903a121847fa40e48a96e60fcb
Winar Owen <owensumargo66@gmail.com>
2026-03-20 21:04:42 +0700

    Changed the load env parameter in settings.py

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-if os.environ.get('RENDER') is None:
+if os.environ.get('RAILWAY_ENVIRONMENT') is None:
     load_dotenv()
 
 # Core settings
